5fc915020e36b40048b498221cea14d34df565d1
emiliagit <emisosa2002@gmail.com>
2024-10-28 22:16:47 -0300

    Enemy Health

--- Assets/Scripts/Enemy/Vida.cs
+++ Assets/Scripts/Enemy/Vida.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Vida : MonoBehaviour
 {
     private float Hp = 10;
-    
+    [SerializeField] private Slider healthSliderEnemy;
+
+    private void Start()
+    {
+        UpdateHealthUI();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Bullet"))
@@ -17,11 +23,19 @@ public class Vida : MonoBehaviour
     public void TakeDamage()
     {
         Hp -= 1;
-        Debug.Log(Hp);
         if (Hp <= 0)
         {
             Destroy(gameObject);         
         }
+        UpdateHealthUI();
     }
+
+    void UpdateHealthUI()
+    {
+        Hp = Mathf.Clamp(Hp, 0, 100);
+        healthSliderEnemy.value = Hp;
+
+    }
+
 
 }
