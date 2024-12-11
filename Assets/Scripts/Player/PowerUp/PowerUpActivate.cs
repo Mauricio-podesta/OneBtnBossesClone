04add50e9559afb038a7e72d91114b7473e10a5c
Mauricio-podesta <145945328+Mauricio-podesta@users.noreply.github.com>
2024-12-11 16:50:25 -0300

    PowerUp
    Solo funciona si previamente hiciste la seleción en el nivel

--- Assets/Scripts/Player/PowerUp/PowerUpActivate.cs
+++ Assets/Scripts/Player/PowerUp/PowerUpActivate.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class PowerUpActivate : MonoBehaviour
 {
-    
+    [SerializeField] private GameObject powerUpObject; 
+    [SerializeField] private GameObject jugador;       
+
     void Start()
     {
-        
+        // Verificar la decisión del jugador desde el Singleton
+        if (LevelsButtons.Instance != null)
+        {
+            if (LevelsButtons.Instance.usePowerUp)
+            {
+                ActivatePowerUp();
+            }
+            else
+            {
+                AddChangeDirection();
+            }
+        }
+        else
+        {
+            Debug.LogWarning("No se encontró la instancia de LevelsButtons.");
+        }
     }
 
-  
-    void Update()
+    private void ActivatePowerUp()
     {
-        
+        if (powerUpObject != null)
+        {
+            powerUpObject.SetActive(true); 
+   
+        }
+        else
+        {
+            Debug.LogWarning("No se asignó un objeto para el Power-Up.");
+        }
+    }
+
+   
+    private void AddChangeDirection()
+    {
+        if (jugador != null)
+        {
+            if (jugador.GetComponent<ChangeDirection>() == null)
+            {
+                jugador.AddComponent<ChangeDirection>(); 
+               
+            }
+            else
+            {
+                Debug.Log("El jugador ya tiene el script ChangeDirection.");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("No se asignó el objeto jugador.");
+        }
     }
 }
--- Assets/Scripts/Ui Pantallas/LevelsButtons.cs
+++ Assets/Scripts/Ui Pantallas/LevelsButtons.cs
@@ -2,20 +2,83 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelsButtons : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static LevelsButtons Instance;
+    [SerializeField] private GameObject choicePanel;
+    [SerializeField] private Button powerUpButton;  
+    [SerializeField] private Button noPowerUpButton;
+    [SerializeField] private Button cancelButton;
 
-    // Update is called once per frame
-    void Update()
+    private AudioSource audioSource;
+
+    public bool usePowerUp { get; private set; }
+    public void GetInstance()
     {
-        
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
     }
+    private void Awake()
+    {
+        GetInstance();
+
+        if (powerUpButton != null)
+        {
+            powerUpButton.onClick.AddListener(() => SelectOption(true));
+        }
+        if (noPowerUpButton != null)
+        {
+            noPowerUpButton.onClick.AddListener(() => SelectOption(false));
+        }
+        if (cancelButton != null)
+        {
+            cancelButton.onClick.AddListener(CloseChoicePanel);
+        }
+
+        audioSource = gameObject.AddComponent<AudioSource>();
+    }
+    public void ShowChoicePanel()
+    {
+        if (choicePanel != null)
+        {
+            choicePanel.SetActive(true);
+        }
+    }
+    private void CloseChoicePanel()
+    {
+        if (choicePanel != null)
+        {
+            choicePanel.SetActive(false);
+        }
+    }
+    
+        private void SelectOption(bool usePowerUpOption)
+        {
+            usePowerUp = usePowerUpOption;
+
+            if (usePowerUp)
+            {
+                Debug.Log("Movimiento con Power-Up seleccionado");
+            }
+            else
+            {
+                Debug.Log("Movimiento sin Power-Up seleccionado");
+            }
+
+            ScenePlay();
+        }
+
+    
+
     public void ScenePlay()
     {
         SceneManager.LoadScene("GameScene");
